cf6f11fa3785ae61d5efa6f0b4398240d87dd138
soojungkk <sj921101@naver.com>
2022-10-10 09:26:59 +0900

    refactor : 전략클래스 인터페이스 추가 및Override  메소드 구현 추가

--- src/main/java/racingcar/domain/playStrategy/RandomPlay.java
+++ src/main/java/racingcar/domain/playStrategy/RandomPlay.java
@@ -2,10 +2,11 @@ package racingcar.domain.playStrategy;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class RandomPlay {
+public class RandomPlay implements PlayBehavior {
 
     private static final int GO_NUM_MIN = 4;
 
+    @Override
     public boolean isGo() {
         return Randoms.pickNumberInRange(0, 9) >= GO_NUM_MIN;
     }
