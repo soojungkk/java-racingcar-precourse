8bdb19dc95144f8499d1723d00f9a792e5a4e056
soojungkk <sj921101@naver.com>
2022-10-10 09:39:34 +0900

    feat : 게임 테스트 클래스 추가 ( 자동차 셋팅 및 이동 테스트 코드 추가)

--- src/test/java/racingcar/domain/RacingGameTest.java
+++ src/test/java/racingcar/domain/RacingGameTest.java
@@ -1,4 +1,67 @@
 package racingcar.domain;
 
-public class RacingGameTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.car.Car;
+import racingcar.domain.playStrategy.RandomPlay;
+import racingcar.dto.RacingGameInfo;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RacingGameTest {
+
+    private final String[] carNames = new String[]{"pobi", "woni"};
+    private RacingGameInfo racingGameInfo;
+
+    @BeforeEach
+    void setUp() {
+        racingGameInfo = new RacingGameInfo(new String[]{"pobi", "woni"}, 2);
+    }
+
+    @Test
+    void RacingGame_셋팅() {
+        RacingGame racingGame = new RacingGame(racingGameInfo);
+
+        List<Car> carList = racingGame.getCarList();
+        assertThat(carList.size()).isEqualTo(carNames.length);
+
+        for (int i = 0; i < carNames.length; i++) {
+            assertThat(carList.get(i)).isEqualTo(new Car(carNames[i]));
+        }
+    }
+
+    @Test
+    void 자동차들_이동() {
+        RacingGame racingGame = new RacingGame(racingGameInfo);
+        racingGame.go(new RandomPlay() {
+            @Override
+            public boolean isGo() {
+                return true;
+            }
+        });
+
+        List<Car> carList = racingGame.getCarList();
+        for (Car car : carList) {
+            assertThat(car.getPosition()).isEqualTo(1);
+        }
+    }
+
+    @Test
+    void 자동차들_이동_안함() {
+        RacingGame racingGame = new RacingGame(racingGameInfo);
+        racingGame.go(new RandomPlay() {
+            @Override
+            public boolean isGo() {
+                return false;
+            }
+        });
+
+        List<Car> carList = racingGame.getCarList();
+        for (Car car : carList) {
+            assertThat(car.getPosition()).isEqualTo(0);
+        }
+    }
+
 }
