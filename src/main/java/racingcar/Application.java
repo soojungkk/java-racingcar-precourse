3540b9e1b95b20e7e3a6db5ae7979e6da11b39ee
soojungkk <sj921101@naver.com>
2022-10-10 09:49:13 +0900

    feat : 자동차 게임 main 로직 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,28 @@
 package racingcar;
 
+import racingcar.domain.Winners;
+import racingcar.domain.playStrategy.RandomPlay;
+import racingcar.dto.RacingGameInfo;
+import racingcar.domain.RacingGame;
+import racingcar.ui.InputView;
+import racingcar.ui.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // input racingCar names and tryCount
+        RacingGameInfo racingGameInfo = InputView.getRacingGameInfo();
+
+        RacingGame racingGame = new RacingGame(racingGameInfo);
+        int tryTime = racingGameInfo.getTryTime();
+
+        for (int i = 0; i < tryTime; i++) {
+            racingGame.go(new RandomPlay());
+
+            // print racingCar move
+            OutputView.printRacingGame(i, racingGame.getCarList());
+        }
+
+        // print winners
+        OutputView.printWinners(Winners.getWinners(racingGame.getCarList()));
     }
 }
